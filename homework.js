a382a4b66d0601f428910d03b5dc600dfbbe381f
Alla Klimova <alla_88@inbox.lv>
2017-01-20 20:11:53 -0300

    add Tennessee

--- homework.js
+++ homework.js
@@ -1,9 +1,14 @@
 "use strict";
+const States = require('./statesClass');
+const states = require('./states');
 
 // Этот код можно менять как угодно
 
 
 const TaxCalculator = require('./taxcalculator');
+
+var newState = new States(states);
+newState.addNewState("Tennessee", 0.05, "",  "", 0.07);
 
 //############################
 //Production - код:
@@ -13,15 +18,17 @@ calculateTaxes();
 //Тесты:
 
 var tests = [
-    () => assertEquals(3.0 * (1 + 0.04),  new TaxCalculator().calculatePriceFor("Alabama", "eggs")),
-    () => assertEquals(0.4 * (1 + 0.015 + 0.065),  new TaxCalculator().calculatePriceFor("Arkansas", "coca-cola")),
-    () => assertEquals(6.7 * (1 + 0.0),  new TaxCalculator().calculatePriceFor("Alaska", "amoxicillin")),
-    () => assertEquals(6.7 * (1 + 0.0),  new TaxCalculator().calculatePriceFor("California", "amoxicillin")),
-    () => assertEquals(2 * (1 + 0.0635),  new TaxCalculator().calculatePriceFor("Connecticut", "hamburger")),
+    () => assertEquals(3.0 * (1 + 0.04), new TaxCalculator().calculatePriceFor("Alabama", "eggs")),
+    () => assertEquals(0.4 * (1 + 0.015 + 0.065), new TaxCalculator().calculatePriceFor("Arkansas", "coca-cola")),
+    () => assertEquals(6.7 * (1 + 0.0), new TaxCalculator().calculatePriceFor("Alaska", "amoxicillin")),
+    () => assertEquals(6.7 * (1 + 0.0), new TaxCalculator().calculatePriceFor("California", "amoxicillin")),
+    () => assertEquals(2 * (1 + 0.0635), new TaxCalculator().calculatePriceFor("Connecticut", "hamburger")),
+    () => assertEquals(2 * (1 + 0.07), new TaxCalculator().calculatePriceFor("Tennessee", "hamburger")),
+    () => assertEquals(0.4 * (1 + 0.07 + 0.05), new TaxCalculator().calculatePriceFor("Tennessee", "coca-cola")),
 ];
 
 //Раскомментируйте следующую строчку для запуска тестов:
-runAllTests (tests);
+runAllTests(tests);
 
 //############################
 //Код ниже этой строчки не надо менять для выполнения домашней работы
