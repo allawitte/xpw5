9568bec7b9e79aef110458cd46e5f51eccbe1a7f
Alla Klimova <alla_88@inbox.lv>
2017-01-19 22:46:24 -0300

    removed class State from homework.js

--- homework.js
+++ homework.js
@@ -23,42 +23,6 @@ var states = {
     "Connecticut":{"Groceries": "", "PrescriptionDrug": "", "base":0.0635}
 };
 
-class States {
-    constructor(states){
-        this._states = states
-    }
-
-    get statesNames(){
-        var arr = [];
-        for(var key in this._states) {
-            arr.push(key);
-        }
-        return arr;
-    }
-    getState(state){
-        return this._states[state];
-    }
-
-    get randomState(){
-        return this.statesNames[Math.floor(Math.random() * this.statesNames.length)];
-    }
-    getBase(state){
-        return this._states[state].base;
-    }
-    getStateTax(state, itemType){
-        return this.getState(state)[itemType];
-    }
-
-    calcNoBasicTax(state, itemType){
-        if (this.getStateTax(state, itemType) === ""){
-            return 0
-        }
-        return this.getBase(state) + this.getStateTax(state, itemType);
-
-    }
-}
-
-
 const TaxCalculator = require('./taxcalculator');
 
 
