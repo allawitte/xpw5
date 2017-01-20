7a8a907db8269459101f46c0a0f10a3f4a680021
Alla Klimova <alla_88@inbox.lv>
2017-01-19 22:58:13 -0300

    states removed from homework

--- homework.js
+++ homework.js
@@ -2,15 +2,6 @@
 
 // Этот код можно менять как угодно
 
-var states = {
-    "Alabama":{"Groceries": 0, "PrescriptionDrug": "", "base":0.04},
-    "Alaska":{"Groceries": 0, "PrescriptionDrug": "", "base":0},
-    "Arizona":{"Groceries": "", "PrescriptionDrug": "", "base":0.056},
-    "Arkansas":{"Groceries": 0.015, "PrescriptionDrug": "", "base":0.065},
-    "California":{"Groceries": "", "PrescriptionDrug": "", "base":0.075},
-    "Colorado":{"Groceries": "", "PrescriptionDrug": "", "base":0.029},
-    "Connecticut":{"Groceries": "", "PrescriptionDrug": "", "base":0.0635}
-};
 
 const TaxCalculator = require('./taxcalculator');
 
